7c345ef514dade21d05fd54d84a08fe49b565b5b
yessimov-a <a.yessimov98@gmail.com>
2019-10-12 14:34:53 +0600

    feat: complete js-edu project

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      return knowsProgramming ? Math.ceil( 800 / config[focus]) : Math.ceil( 1300 / config[focus])
   };
   
